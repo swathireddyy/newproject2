d88c14924bf74dd23e9fbc2d398a05e80c9cff39
swathireddyy <swathithillana12@gmail.com>
2020-07-05 13:19:12 +0530

    calc program added

---
+++ calc.java
@@ -0,0 +1,15 @@
+class clac{
+	public static void main(String[] args){
+	System.out.println("calculator prgram");
+}
+	public int add(){
+		int a=5;
+		int b=10;
+		return (a+b);
+}
+	public int sub(){
+		int a=20;
+		int b=10;
+		return (a-b);
+}
+}
